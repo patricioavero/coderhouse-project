ec3243b4fc0be311c3b62c2e8e8d024d58cb62f4
PATO <patricio.avero@gmail.com>
2022-01-27 22:48:57 -0300

    Some minor changes

--- crud/views.py
+++ crud/views.py
@@ -4,5 +4,8 @@ from .models import member
 # Create your views here.
 
 def home(request):
+    return render(request, 'home.html')
+
+def members(request):
     r_members_list = member.objects.all()
-    return render(request, 'home.html', {"members": r_members_list}) 
+    return render(request, 'members.html', {"members": r_members_list})
